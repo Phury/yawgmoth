3bf6c3bb415d577c2b57ab33bf56f29dc899d964
phatarhs <PierreHenri.Vandevelde@arhs-technology.com>
2018-12-26 12:59:24 +0100

    dist gh-pages

--- src/app/app.config.js
+++
@@ -1,9 +0,0 @@
-export function routing($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise("/editor");
-    $stateProvider
-        .state('base', {
-            url: '',
-            abstract: true,
-            template: '<div ui-view></div>'
-        });
-}
